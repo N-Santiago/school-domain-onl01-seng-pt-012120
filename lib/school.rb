e3c1cdfc647f1a31ae779b6918097c4c7093cfdc
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-07 03:18:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 class School 
   attr_accessor 
-  attr_reader :schoolname, :roster 
+  attr_reader :schoolname, :roster, :student_name, :grade  
   
   def initialize(school_name)
     @school_name = school_name
@@ -8,5 +8,8 @@ class School
   end
   
   def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+  end 
     
 end 
