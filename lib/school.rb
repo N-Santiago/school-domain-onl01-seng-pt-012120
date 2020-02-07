9bcd44dd0cb37053a6314ab72af9b35021232a83
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-07 03:23:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,11 @@ class School
   
   def add_student(student_name, grade)
     if @roster[grade]
+       @roster[grade] << student_name
+  else
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end
   end 
     
 end 
